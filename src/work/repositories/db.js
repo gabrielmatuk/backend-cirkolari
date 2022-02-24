1076e8db849776750f2e2cccd50161f1998ca44e
gabrielmatuk <63329596+gabrielmatuk@users.noreply.github.com>
2022-02-24 17:08:51 -0300

    Update db.js

--- src/work/repositories/db.js
+++ src/work/repositories/db.js
@@ -2,7 +2,7 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 const connectionString =
-  "postgres://kbuceotx:eE1ZLG9cIaojQ5xb2YaLnSSkQkal9B5t@castor.db.elephantsql.com/kbuceotx";
+  "";
 const connect = async () => {
   const pool = new Pool({
     connectionString,
